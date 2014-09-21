293444f8b171a57acd0094b400a037e7e47cc272
Josh Cai <yuanshi419@gmail.com>
2014-09-20 22:13:39 -0500

    cleaned up code, moved static files to separate folder

--- static_server.js
+++ static_server.js
@@ -6,66 +6,57 @@ var path = require('path');
 var static = require('node-static');
 var multiparty = require('multiparty');
 
-var file = new static.Server();
+var file = null;
+// if 'uploads' directory doesn't exist, make it
+fs.stat('./uploads', function(err, stats) {
+  if(!stats) {
+    fs.mkdirSync('./uploads');
+  }
+  file = new static.Server('./uploads');
+});
 
 http.createServer(function (req, res) {
 
     switch (req.url) {
       case '/upload':
-        display_form(req, res);
+        // display upload form
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(
+          '<form action="/uploaded" enctype="multipart/form-data" method="post">'+
+          '<input type="file" name="upload" multiple="multiple"><br>'+
+          '<input type="submit" value="Upload">'+
+          '</form>'
+        );
         break;
       case '/uploaded':
+        // process uploaded document
         var form = new multiparty.Form();
 
-        var fileName = '';
-        form.on('part', function(part){
-            if(!part.filename) return;
-            fileName = part.filename;
-        });
-
-        form.on('file', function(name, file){
-          var target_path = path.join(__dirname, fileName);
-          fs.renameSync(file.path, target_path, function(err){
+        form.on('file', function(name, file) {
+          var target_path = path.join(__dirname, 'uploads', file.originalFilename);
+          // rename file from temp location
+          fs.renameSync(file.path, target_path, function(err) {
             if(err) console.error(err.stack);
           });
         });
 
         form.parse(req, function(err, fields, files) {
-          res.writeHead(301,
-            {Location: '/list'}
-          );
+          // redirect to '/list'
+          res.writeHead(301, {Location: '/list'});
           res.end();
         });
-        return;
         break;
-      case '/list':
-        var files = fs.readdirSync('.');
-        files.splice(files.indexOf('node_modules'), 1);
-        files.splice(files.indexOf('package.json'), 1);
-        files.splice(files.indexOf('static_server.js'), 1);
-        files.splice(files.indexOf('.git'), 1);
-        files.splice(files.indexOf('.gitignore'), 1);
+      case '/list': // list all available files on server
+        var files = fs.readdirSync('./uploads');
 
-        var html = '';
-        for(var i=0; i < files.length; i++)
-        {
-          html += '<a href="/' + files[i] + '">' + files[i] + '</a><br>';
-        }
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html);
+        files.forEach(function(file) {
+          res.write('<a href="/' + file + '">' + file + '</a><br>');
+        });
+        res.end();
         break;
       default:
         file.serve(req, res); // serve static file
     }
 
 }).listen(4001);
-
-function display_form(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(
-      '<form action="/uploaded" enctype="multipart/form-data" method="post">'+
-      '<input type="file" name="upload" multiple="multiple"><br>'+
-      '<input type="submit" value="Upload">'+
-      '</form>'
-    );
-}
